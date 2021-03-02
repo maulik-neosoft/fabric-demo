7f1ef6a1b606f175ecc0fb0da30a3c015c66e17b
Maulik Bhojani <maulikbhojani3@gmail.com>
2021-03-02 15:50:30 +0530

    added download and image option

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
   <div id="app">
     <canvas ref="can" class="fabric-canvas"></canvas>
     <input v-model="text" style="margin-top: 10px;" /><button @click="addText">Add Text</button>
+    <button @click="handleDownload">Download</button>
   </div>
 </template>
 
 <script>
 import { fabric } from "fabric";
-
-console.log("fabric : ", fabric);
 
 export default {
   data() {
@@ -26,12 +25,17 @@ export default {
       height: 50
     });
     this.canvas.add(rect);
+
+    fabric.Image.fromURL("http://fabricjs.com/assets/pug_small.jpg", (myImg) => {
+      const img1 = myImg.set({ left: 0, top: 0, width: 150, height: 150 });
+      this.canvas.add(img1);
+    });
   },
   methods: {
     addText() {
       const _text = new fabric.IText(this.text, {
         fontFamily: "arial black",
-        fontSize: 10
+        fontSize: 18
       });
 
       // const text = new fabric.Text(this.text, {
@@ -40,6 +44,12 @@ export default {
 
       this.canvas.add(_text);
       this.$forceUpdate();
+    },
+    handleDownload() {
+      const link = document.createElement("A");
+      link.setAttribute("download", "image.png");
+      link.setAttribute("href", this.canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+      link.click();
     }
   }
 };
